6ab92e3b545fc6f73ff1b5f2276a4e923970ca9a
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2020-07-30 11:52:08 +0300

    Get heroes with HttpClient

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -21,9 +21,7 @@ export class HeroService {
   private heroesUrl = 'api/heroes';
 
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
