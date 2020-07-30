89190c040b15ec5b6ee22b79112a955dbc213b6c
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2020-07-30 11:35:14 +0300

    Extract the id route parameter

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -19,5 +19,12 @@ export class HeroDetailComponent implements OnInit {
     private location: Location
   ) {}
 
-  ngOnInit(): void {}
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
 }
