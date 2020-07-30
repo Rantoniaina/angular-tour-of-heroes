d87acebe5e7cb8ed5b1b5e4a821870fba68cc05c
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2020-07-30 10:43:13 +0300

    Master/Detail

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
@@ -9,14 +8,14 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
-
   heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit() {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
